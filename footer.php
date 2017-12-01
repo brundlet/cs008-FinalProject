c25c52326779528efdfe5dd84bcf8e2f3dd766bf
asnolin <33637959+asnolin@users.noreply.github.com>
2017-12-01 12:07:42 -0500

    image makes it look legit

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
 <footer>
 	<p>Designed by Brian Rundlet, Andrew Nolin, and Michael Beale</p>
 </footer>
-
+<img src=https://ixquick-proxy.com/do/spg/show_picture.pl?l=english&rais=1&oiu=https%3A%2F%2Fthumbs.dreamstime.com%2Fb%2Fsecure-icon-white-background-51433787.jpg&sp=1433d040c4b3ceee11379523aaa518bb", alt="">
 </body>
 </html>
