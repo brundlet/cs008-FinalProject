c245a94a5f84b3c9f1cd8bbc0c23a9e0643bb98e
michaelbeale16 <33990517+michaelbeale16@users.noreply.github.com>
2017-12-01 19:47:42 -0500

    added text

--- prevent.php
+++ prevent.php
@@ -8,7 +8,30 @@ include('top.php');
                 <li>Be aware of suspicious sites. Many sites are designed to appear legitimate, but really have sinister intent. Always 
                     be cautious when entering sensitive information into a new site.</li>
             </ol>
-            
+            <ol>
+                <li>Be on the lookout for fake “phishing” emails, calls or texts that claim to come from your bank but are in fact sent 
+                    for nefarious purposes.  If something seems off, take a closer look.</li>
+            </ol>
+            <ol>
+                <li>Keep important financial documents in a safe place in your house.  When you no longer need them, never leave such 
+                    documents in the trash or recycling where anyone could get to them.  Always shred the documents first.</li>
+            </ol>
+            <ol>
+                <li>Bits of information ranging from your date of birth to your mother’s maiden name are often used as “security 
+                    questions” to confirm your identity.  Try to avoid posting such information on social networking and other websites.
+                    In the wrong hands, it could be just what someone needs to impersonate you.</li>
+            </ol>
+            <ol>
+                <li>Anyone who gets hold of your credit card can use it however they want.  Be very careful with your purse or wallet 
+                    and never leave it unattended.  Never carry things like your Social Security card or written down PIN numbers.  If 
+                    your credit card is lost or stolen, cancel that card IMMEDIATELY.</li>
+            </ol>
+            <ol>
+                <li>It is of course important to make sure that your anti-virus and anti-spyware software on your computer is updated 
+                    regularly, but do not forget to apply encryption and security applications to your mobile devices, such as smart phones 
+                    and tablets, and even flash drives containing sensitive information.  These devices can be lost or stolen just as easily 
+                    as your credit card, and need to be protected just as much.</li>
+            </ol>
 </article>
 <?php
 include('footer.php');
