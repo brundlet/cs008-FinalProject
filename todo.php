962bedeca0f61eb1c6ecbda6d1a2fd4b8c521d1e
michaelbeale16 <33990517+michaelbeale16@users.noreply.github.com>
2017-12-04 11:58:17 -0500

    updated text

--- todo.php
+++ todo.php
@@ -5,6 +5,26 @@ include('top.php');
             <h2 class="pages">What Do I Do Once My Identity is Stolen?</h2>
             <p>There are numerous crucial steps you must take if you suspect that your identity is stolen.
             </p>
+            <ol>
+                <li>The first thing to do if an account is being accessed is to contact the bank or 
+                    financial institution and close the account in question or place it on lockdown 
+                    to avoid further deductions.
+                </li>
+            </ol>
+            <ol>
+                <li>Sign up for a credit monitoring service -- many companies offer one for free to 
+                    victims of database leaks.</li>
+            </ol>
+            <ol>
+                <li>Check over older bank statements and credit reports for charges and accounts 
+                    that you do not recognize.  The identity theft may have occurred some time 
+                    ago and was simply not noticed.</li>
+            </ol>
+            <ol>
+                <li>Identity theft is a very serious crime and the proper authorities should be 
+                notified.  Call your local police department and file a report with the Federal 
+                Trade Commission.</li>
+            </ol>
 </article>
 
 <?php
