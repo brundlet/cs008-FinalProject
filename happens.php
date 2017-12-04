baa4671fc14338096bb6b36645323d8d3d2db2b9
michaelbeale16 <33990517+michaelbeale16@users.noreply.github.com>
2017-12-04 12:27:53 -0500

    updated text

--- happens.php
+++ happens.php
@@ -3,9 +3,31 @@ include('top.php');
 ?>
 <article>
             <h2 class="pages">What Happens Once My Identity is Stolen?</h2>
-            <p>Identity theft is an extremely serious matter. Once hackers obtain your vital information like birthdate, social security number,
-                email, password, home address, and zip code, they can instantly assume your identity. 
+            <p>Identity theft is an extremely serious matter. Once hackers obtain your vital information like date of birth, social security number,
+                email, password, home address, and zip code, they can instantly assume your identity. Once that happens, they can use your credit 
+                card and bank account indiscriminately, siphoning money from your bank account and potentially ruining your credit report.  Here 
+                are just a few of the potential adverse effects:  
             </p>
+            <ol>
+                <li>A bad credit report can make it harder to make large purchases, increase your insurance rates, and even make it harder to 
+                get a job. </li>
+            </ol>
+            <ol>
+                <li>If your social security number is stolen, they could draw out credits for social security payments later in your life,
+                and you could even be sent a tax bill for income you did not earn.</li>
+            </ol>
+            <ol>
+                <li>Criminals have been known to give the police a stolen identity upon being arrested.  This can saddle you with a criminal
+                record, which will follow you for the rest of your life.</li>
+            </ol>
+            <ol>
+                <li>If your child's identity is used and gains bad credit, it can go unnoticed for years and cause major problems when 
+                applying for college financial aid.</li>
+            </ol>
+            <ol>
+                <li>Some criminals try to avoid creditors by using a stolen identity and then registering themselves as deceased.  If 
+                you are regarded as dead by a bank's database, it can be surprisingly hard to convince them otherwise.  </li>
+            </ol>
 </article>
 <?php
 include('footer.php');
